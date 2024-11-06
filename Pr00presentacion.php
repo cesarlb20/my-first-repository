2233b7a28c55a634efe866034acd07d3bdf53917
Cesar <cesarlb208@gmail.com>
2024-11-06 10:54:42 -0600

    Add files via upload

---
+++ Pr00presentacion.php
@@ -0,0 +1,11 @@
+<?php
+/* CBTis 89
+programa que  
+Cesar Limon Blanco
+3.-A Programacion Matutino
+*/
+
+echo "Mi nombre es cesar limon blanco";
+echo "<br>""Estuido en el CBTis 89 en durango";
+echo "<br>""en 3er semestre de kla carrrera de programacion A matutino";
+echo "vivo en la ciudad de durango";
